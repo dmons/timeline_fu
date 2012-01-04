c883c1f8480bb0cc95d40da7e8e9c3d564c5dd29
mikefh <michael.herrera@gmail.com>
2012-01-03 22:08:04 -0800

    Update generators/timeline_fu/templates/migration.rb

--- generators/timeline_fu/templates/migration.rb
+++ generators/timeline_fu/templates/migration.rb
@@ -1,13 +1,12 @@
 class CreateTimelineEvents < ActiveRecord::Migration
   def self.up
     create_table :timeline_events do |t|
-      t.references :account
-      t.string   :event_type, :subject_type,  :actor_type,  :secondary_subject_type,  :extra_scope_type
-      t.integer               :subject_id,    :actor_id,    :secondary_subject_id,    :extra_scope_id
+      t.string   :event_type, :account_type, :subject_type,  :actor_type,  :secondary_subject_type,  :extra_scope_type
+      t.integer               :account_id,   :subject_id,    :actor_id,    :secondary_subject_id,    :extra_scope_id
       t.timestamps
     end
 
-    add_index :timeline_events, :account_id
+    add_index :timeline_events, [:account_id , :account_type]
     add_index :timeline_events, [:subject_id , :subject_type]
     add_index :timeline_events, [:actor_id , :actor_type]
     add_index :timeline_events, [:secondary_subject_id , :secondary_subject_type], :name => 'secondary_subject_timeline_events'
