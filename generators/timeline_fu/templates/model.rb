a1835681727b0e40525ade1ebc0a50c12fd98c03
mikefh <michael.herrera@gmail.com>
2012-01-03 22:03:17 -0800

    Changed the :account to be polymorphic.

--- generators/timeline_fu/templates/model.rb
+++ generators/timeline_fu/templates/model.rb
@@ -1,7 +1,7 @@
 class TimelineEvent < ActiveRecord::Base
-  belongs_to :account
+  belongs_to :account,            :polymorphic => true
   belongs_to :actor,              :polymorphic => true
   belongs_to :subject,            :polymorphic => true
   belongs_to :secondary_subject,  :polymorphic => true
-  belongs_to :extra_scope,              :polymorphic => true
+  belongs_to :extra_scope,        :polymorphic => true
 end
